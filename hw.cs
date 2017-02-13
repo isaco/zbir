814e74c096c906997a393ce6f2e9218f8efa4bbd
isaco <isaco@users.noreply.github.com>
2017-02-13 13:04:39 +0100

    Create hw.cs

---
+++ hw.cs
@@ -0,0 +1,15 @@
+using System;
+namespace Hw
+{
+    class Hw
+    {
+        static void Main() 
+        {
+            Console.WriteLine("Hello World!");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
